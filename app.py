1933b554ed48f910aab885df700ff3d62c8842cd
Sylwester Sojka <sylwestersojka@Sylwesters-MacBook-Air.local>
2023-12-31 13:22:48 +0100

    Adding captcha fields and testing mode but disabled now

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from flask_wtf import FlaskForm
+from flask_wtf import FlaskForm, RecaptchaField
 from wtforms import StringField, PasswordField, IntegerField, BooleanField, Form, FormField, FieldList
 from wtforms.validators import InputRequired, Length, AnyOf, Email
 from collections import namedtuple
@@ -7,6 +7,9 @@ from collections import namedtuple
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'MySecret!'
+app.config['RECAPTCHA_PUBLIC_KEY'] = '6LdYkkEpAAAAAEPkccUCcDzF5ixijD7Gqeezzhvb'
+app.config['RECAPTCHA_PRIVATE_KEY'] = '6LdYkkEpAAAAAFG8F5y9lZuaWocY_TLtitUsobt5'
+app.config['TESTING'] = False
 
 
 class YearForm(Form):
@@ -42,6 +45,7 @@ class NameForm(LoginForm):
     home_phone = FormField(TelephoneForm)
     mobile_phone = FormField(TelephoneForm)
     years = FieldList(FormField(YearForm), min_entries=3)
+    recaptcha = RecaptchaField('recaptcha')
 
 class User():
     def __init__(self, username, age, email) -> None:
@@ -49,38 +53,56 @@ class User():
         self.age = age
         self.email = email
 
+class DynamicForm(FlaskForm):
+    pass
+
+@app.route('/dynamic', methods = ['POST', 'GET'])
+def dynamic():
+    DynamicForm.name = StringField('name')
+
+    names = ['Name', 'Second_name', 'Nick_name']
+    for name in names:
+        setattr(DynamicForm, name, StringField(name))
 
 
 
-    @app.route('/', methods = ['POST', 'GET'])
-    def index():
+    form = DynamicForm()
 
-       
+    if form.validate_on_submit():
+        return f'Name is: {form.name.data} Nickname is: {form.Nick_name.data}'
+    
+    return render_template('dynamic.html', form=form, names=names)
+    
 
-        group = namedtuple('Group', ['year', 'total'])
-        g1 = group(2020, 100)
-        g2 = group(2021, 200)
-        g3 = group(2022, 300)
+@app.route('/', methods = ['POST', 'GET'])
+def index():
 
-        data = {'years' : [g1, g2, g3]}
+    
 
-        User_sylwek = User(username='Stalonek', age=32, email='cykor13@gmail.com')
-        form = NameForm(obj=User_sylwek, data=data)
+    group = namedtuple('Group', ['year', 'total'])
+    g1 = group(2020, 100)
+    g2 = group(2021, 200)
+    g3 = group(2022, 300)
 
-        del form.password
+    data = {'years' : [g1, g2, g3]}
 
-        if form.validate_on_submit():
-            # return f'Home Phone area code: {form.home_phone.area_code.data} and Mobile area code is: {form.mobile_phone.area_code.data}'
-            # return f'<h1> Username: {form.username.data} password: {form.password.data} Age: {form.age.data} Yes or No: {form.yes_no.data} email: {form.email.data}</h1>'
-            output = '<h1>'
+    User_sylwek = User(username='Stalonek', age=32, email='cykor13@gmail.com')
+    form = NameForm(obj=User_sylwek, data=data)
 
-            for field in form.years:
-                output += f'Year: {field.year.data} Total: {field.total.data} <br />' 
-                
-            output +=  '</h1>'
-            return output
+    del form.password
 
-        return render_template('index.html', form=form)
+    if form.validate_on_submit():
+        # return f'Home Phone area code: {form.home_phone.area_code.data} and Mobile area code is: {form.mobile_phone.area_code.data}'
+        # return f'<h1> Username: {form.username.data} password: {form.password.data} Age: {form.age.data} Yes or No: {form.yes_no.data} email: {form.email.data}</h1>'
+        output = '<h1>'
+
+        for field in form.years:
+            output += f'Year: {field.year.data} Total: {field.total.data} <br />' 
+            
+        output +=  '</h1>'
+        return output
+
+    return render_template('index.html', form=form)
 
 
 
