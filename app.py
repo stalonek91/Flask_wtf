1a62d7f8a51369a6814786cbc57647c5ea81251b
Sylwester Sojka <sylwestersojka@Sylwesters-MacBook-Air.local>
2023-12-31 09:11:36 +0100

    Adding FieldList and named tuple functionality

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, IntegerField, BooleanField, Form, FormField
+from wtforms import StringField, PasswordField, IntegerField, BooleanField, Form, FormField, FieldList
 from wtforms.validators import InputRequired, Length, AnyOf, Email
+from collections import namedtuple
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'MySecret!'
+
+
+class YearForm(Form):
+    year = IntegerField('Year')
+    total = IntegerField('Total')
 
 class TelephoneForm(Form):
     country_code = StringField('Country code:')
@@ -35,6 +41,7 @@ class NameForm(LoginForm):
     second_name = StringField('2nd Name')
     home_phone = FormField(TelephoneForm)
     mobile_phone = FormField(TelephoneForm)
+    years = FieldList(FormField(YearForm), min_entries=3)
 
 class User():
     def __init__(self, username, age, email) -> None:
@@ -44,16 +51,33 @@ class User():
 
 
 
+
     @app.route('/', methods = ['POST', 'GET'])
     def index():
 
+       
+
+        group = namedtuple('Group', ['year', 'total'])
+        g1 = group(2020, 100)
+        g2 = group(2021, 200)
+        g3 = group(2022, 300)
+
+        data = {'years' : [g1, g2, g3]}
+
         User_sylwek = User(username='Stalonek', age=32, email='cykor13@gmail.com')
-        form = NameForm(obj=User_sylwek)
+        form = NameForm(obj=User_sylwek, data=data)
 
         if form.validate_on_submit():
-            return f'Home Phone area code: {form.home_phone.area_code.data} and Mobile area code is: {form.mobile_phone.area_code.data}'
+            # return f'Home Phone area code: {form.home_phone.area_code.data} and Mobile area code is: {form.mobile_phone.area_code.data}'
             # return f'<h1> Username: {form.username.data} password: {form.password.data} Age: {form.age.data} Yes or No: {form.yes_no.data} email: {form.email.data}</h1>'
-        
+            output = '<h1>'
+
+            for field in form.years:
+                output += f'Year: {field.year.data} Total: {field.total.data} <br />' 
+                
+            output +=  '</h1>'
+            return output
+
         return render_template('index.html', form=form)
 
 
